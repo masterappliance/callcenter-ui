ff660b038c0f766f45fd3927faea506269278e9f
masterappliance <129891817+masterappliance@users.noreply.github.com>
2025-11-13 12:00:16 -0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // CallRail-ish palette
+        primary: {
+          DEFAULT: '#2D5FE4'
+        }
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
