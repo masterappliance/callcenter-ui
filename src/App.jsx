d06e7b6e911081ab8833d9e9fa28c0750026b13a
masterappliance <129891817+masterappliance@users.noreply.github.com>
2025-11-13 13:05:37 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import AppShell from './layouts/AppShell.jsx'
-import CallsPage from './pages/CallsPage.jsx'
+import { Routes, Route, Navigate } from "react-router-dom";
+import AppShell from "./layouts/AppShell.jsx";
+import CallsLayout from "./pages/CallsLayout.jsx";
 
 export default function App() {
   return (
     <AppShell>
       <Routes>
         <Route path="/" element={<Navigate to="/calls" replace />} />
-        <Route path="/calls" element={<CallsPage />} />
-        <Route path="/calls/:id" element={<CallsPage />} />
+        {/* All call-related views live under /calls */}
+        <Route path="/calls/*" element={<CallsLayout />} />
       </Routes>
     </AppShell>
-  )
+  );
 }
