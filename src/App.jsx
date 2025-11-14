b5b964551fef340791c917379be39a0e7c418990
masterappliance <129891817+masterappliance@users.noreply.github.com>
2025-11-14 10:33:53 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 // src/App.jsx
-
-import { Routes, Route, Navigate } from "react-router-dom";
-import AppShell from "./layouts/AppShell.jsx";
-import CallsLayout from "./pages/CallsLayout.jsx";
-import DialerLayout from "./pages/DialerLayout.jsx";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AppShell from './layouts/AppShell.jsx';
+import CallsLayout from './pages/CallsLayout.jsx';
+import DialerLayout from './pages/DialerLayout.jsx';
 
 export default function App() {
   return (
     <AppShell>
       <Routes>
-        <Route path="/" element={<Navigate to="/calls" replace />} />
+        {/* Default: open dialer (with left panel) */}
+        <Route path="/" element={<Navigate to="/dialer" replace />} />
 
-        {/* Calls view */}
+        {/* Calls history / details view (if you open it directly) */}
         <Route path="/calls" element={<CallsLayout />} />
         <Route path="/calls/:id" element={<CallsLayout />} />
 
-        {/* Dialer view */}
+        {/* Dialer view (with left recent calls panel) */}
         <Route path="/dialer" element={<DialerLayout />} />
       </Routes>
     </AppShell>
